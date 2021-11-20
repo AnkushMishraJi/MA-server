45ff63d96a7e9343fc60759b24106cb407a04f91
AnkushMishraJi <66322979+AnkushMishraJi@users.noreply.github.com>
2021-11-21 03:49:41 +0530

    Delete server directory

--- server/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://sarfaraz:LLrh7NtBqudx4LMT@cluster0.zk8lr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"hgfkykjbkj"
-  
-};
